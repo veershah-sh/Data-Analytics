99a367f982f645f096b82725e6b0de5c10b739c4
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-02-10 06:02:02 -0800

    added datatypes.py

---
+++ Python/datatypes.py
@@ -0,0 +1,21 @@
+# integer -> 10,0,-8789,4551
+
+# float(floating point number) -> 145.26, 0.254, -1545.652, 10.00
+
+# string -> '', "" 
+    # set of characters 
+    # "jay", 'india', 'abc@123', " "
+
+# boolean -> True, False 
+
+# null -> NULL (empty)
+
+age = 15
+
+price = 854.23
+
+place = "USA"
+
+isPresent = False
+
+bag = ""
