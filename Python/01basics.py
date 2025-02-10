1cfaa1daf1abb8d73232f221493297fc054e6c68
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-02-10 06:01:39 -0800

    Create 01basics.py

---
+++ Python/01basics.py
@@ -0,0 +1,13 @@
+# variable
+# variable declaration
+name = "xyz"
+
+# constant
+# constant declaration
+PRICE = 55.25
+
+print(name)
+print(PRICE)
+
+# to run a python file/program
+# python [file_name]
