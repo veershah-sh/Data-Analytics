776b75cd177f2b5d94a8af3091efdefee4fcef69
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-02-11 05:42:19 -0800

    Update 01basics.py

--- Python/01basics.py
+++ Python/01basics.py
@@ -1,6 +1,7 @@
 # variable
 # variable declaration
 name = "xyz"
+age = 18
 
 # constant
 # constant declaration
@@ -8,6 +9,23 @@ PRICE = 55.25
 
 print(name)
 print(PRICE)
+print("hello we are in class.")
+
+# Rules for naming variables / constants
+# 1. variable name should start with a letter or underscore
+# 2. variable name should not start with a number
+# 3. variable name should only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. variable name should not be reserved words
+# this is in new 
+
+# my_name
+# age_of_apple
+# ageOfApple
+
+# PascalCase -> AgeOfApple, MyAge -> class name
+# camelCase -> ageOfApple, myAge -> variable name, *function name, *Object name
+# snake_case -> age_of_apple, my_age -> variable name
+
 
 # to run a python file/program
 # python [file_name]
