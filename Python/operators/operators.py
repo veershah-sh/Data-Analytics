82b5f0d02a823f877766eaed8565ea35eb413b2a
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-03-11 23:24:04 -0700

    Update operators.py

--- Python/operators/operators.py
+++ Python/operators/operators.py
@@ -29,8 +29,33 @@ Types of Operators:
             -> if a is less than b
             -> if a is equals to b
 
-3. Assignment Operators
+3. Assignment Operators(=)
+    a = 10
+    += -> a += b
+    -= -> a -= b
+    *= -> a *= b
+    /= -> a /= b
+    //= -> a //= b
+    %= -> a %= b
+
 4. Logical Operators
+    AND 
+        -> con1 and con2 
+        -> if both conditions are true -> true
+        -> if any one condition is false -> false
+        -> if both conditions are false -> false 
+    OR
+        -> con1 or con2 
+        -> if both conditions are true -> true
+        -> if any one condition is true -> true
+        -> if both conditions are false -> false
+    NOT
+        -> inverse of and, or operators
+        -> c1 and c2 -> true
+        -> !(c1 and c2) -> false
+        -> c1 or c2 -> false
+        -> !(c1 or c2) -> true
+
 5. Bitwise Operators
 
 6. Membership Operators
