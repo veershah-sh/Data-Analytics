2cc7a5c3d4eb569b4ea838af609c3f9eed72ff27
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-03-11 23:24:40 -0700

    Create assignment.py

---
+++ Python/operators/assignment.py
@@ -0,0 +1,30 @@
+"""
+3. Assignment Operators(=)
+    a = 10
+    += -> a += b
+    -= -> a -= b
+    *= -> a *= b
+    /= -> a /= b
+    //= -> a //= b
+    %= -> a %= b
+
+    a += b
+    a + b
+    a = a+b
+"""
+
+num1 = 20
+num2 = 100
+
+# num1+=num2
+
+# num1-=num2
+
+# num1*=num2
+
+num1/=num2
+
+# num1//=num2
+
+# num1%=num2
+print(num1)
