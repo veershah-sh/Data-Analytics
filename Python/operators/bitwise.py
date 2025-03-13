f90ceddcc3800eb52014e767fd3f4d667d676097
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-03-13 06:08:38 -0700

    Create bitwise.py

---
+++ Python/operators/bitwise.py
@@ -0,0 +1,22 @@
+"""
+5. Bitwise Operators -> works on binary level
+    & -> bitwise and
+    | -> bitwise or
+    ~ -> bitwise not
+    >> -> right shift
+    << -> left shift
+    ^ -> bitwise XOR
+"""
+
+a = 10
+b = 2
+
+print(a & b)
+print(a | b)
+print(~b)
+print(a ^ b)
+
+print(a>>2)
+
+# debug
+print(a<<2)
